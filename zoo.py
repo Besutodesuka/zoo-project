aa3601acf71b318875ac8dc537dce12d99bec01a
Besutodesu <60954224+Besutodesuka@users.noreply.github.com>
2024-11-13 08:48:38 +0000

    fix all fualts

--- zoo.py
+++ zoo.py
@@ -1,10 +1,16 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        # fix condition add 0
+        if 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        # fix condition add 20 
+        elif 13 <= age <= 20:
             return 100
-        elif 21 < age <= 60:
+        # fix condition add 21
+        elif 21 <= age <= 60:
             return 150
-        elif age >= 60:
+        # fix condition remove 60
+        elif age > 60:
             return 100
+        # in case that it is not in condition
+        return 0
